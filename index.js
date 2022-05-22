4dff0d60ccf1f0c93ba2f385bd1a66c158ef6d10
bmsani <bmsani6@gmail.com>
2022-05-23 02:46:15 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || '5000';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urnhu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('wood_master').collection('users');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from WoodMaster')
